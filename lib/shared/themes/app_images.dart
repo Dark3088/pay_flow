04e8fbe20061d75d82e9cf9f4afeb62ab4eeff85
Diego Rocha <diegorsf.dns@gmail.com>
2022-04-19 09:03:12 -0300

    Added main app assets
    - Google Fonts Families
    - Colors
    - Images

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,9 @@
+
+
+class AppImages {
+  static const logoFull   = "assets/images/logofull.png";
+  static const googleFull = "assets/images/google.png";
+  static const logoMini   = "assets/images/logomini.png";
+  static const person     = "assets/images/person.png";
+  static const union      = "assets/images/union.png";
+}
