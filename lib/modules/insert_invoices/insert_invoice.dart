0cd7b4a4a0a5fcb938dd42cf9c2461bd0ae03f26
Diego Rocha <diegorsf.dns@gmail.com>
2022-04-26 04:06:09 -0300

    Added masks for formatting texts

--- lib/modules/insert_invoices/insert_invoice.dart
+++ lib/modules/insert_invoices/insert_invoice.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pay_flow/shared/themes/app_colors.dart';
 import 'package:pay_flow/shared/themes/app_text_styles.dart';
 import 'package:pay_flow/shared/widgets/buttons_set/buttons_set_label.dart';
 import 'package:pay_flow/shared/widgets/input_text/input_text_widget.dart';
 
-class InsertInvoice extends StatelessWidget {
-  const InsertInvoice({Key? key}) : super(key: key);
+class InsertInvoice extends StatefulWidget {
+  final String? barcode;
+  const InsertInvoice({Key? key, this.barcode}) : super(key: key);
+
+  @override
+  _InsertInvoiceState createState() => _InsertInvoiceState();
+}
+
+class _InsertInvoiceState extends State<InsertInvoice> {
+  final moneyTextController =
+      MoneyMaskedTextController(leftSymbol: "R\$", decimalSeparator: ",");
+
+  final dueDateMask = MaskedTextController(mask: "00/00/0000");
+
+  final barcodeInputController = TextEditingController();
+
+  @override
+  void initState() {
+    if (widget.barcode != null) {
+      barcodeInputController.text = widget.barcode!;
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,14 +60,17 @@ class InsertInvoice extends StatelessWidget {
                 onChanged: (value) {}),
             InputTextWidget(
                 label: "Vencimento",
+                editingController: dueDateMask,
                 icon: FontAwesomeIcons.circleXmark,
                 onChanged: (value) {}),
             InputTextWidget(
                 label: "Valor",
+                editingController: moneyTextController,
                 icon: FontAwesomeIcons.dollarSign,
                 onChanged: (value) {}),
             InputTextWidget(
                 label: "Código",
+                editingController: barcodeInputController,
                 icon: FontAwesomeIcons.barcode,
                 onChanged: (value) {}),
           ],
