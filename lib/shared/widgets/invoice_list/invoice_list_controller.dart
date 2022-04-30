1c63a3803d3c11468616ed790258710afb89469e
Diego Rocha <diegorsf.dns@gmail.com>
2022-04-30 11:27:44 -0300

    Invoices wouldn't show up after being created - FIXED

--- lib/shared/widgets/invoice_list/invoice_list_controller.dart
+++ lib/shared/widgets/invoice_list/invoice_list_controller.dart
@@ -3,20 +3,24 @@ import 'package:pay_flow/shared/models/invoice_model/invoice_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class InvoiceListController {
+  final invoiceValueNotifier =
+      ValueNotifier<List<InvoiceModel>>(<InvoiceModel>[]);
 
-  final invoiceValueNotifier = ValueNotifier<List<InvoiceModel>>(<InvoiceModel>[]);
+  List<InvoiceModel> get invoiceModelList => invoiceValueNotifier.value;
+  set invoiceModelList(List<InvoiceModel> value) =>
+      invoiceValueNotifier.value = value;
 
-  List<InvoiceModel> get invoiceModelList =>  invoiceValueNotifier.value;
-  set invoiceModelList(List<InvoiceModel> value)  => invoiceValueNotifier.value = value;
+  InvoiceListController() {
+    getInvoices();
+  }
 
   Future<void> getInvoices() async {
     try {
-
       final sharedPrefs = await SharedPreferences.getInstance();
       final invoiceList = sharedPrefs.getStringList("invoices") ?? <String>[];
-      
-      invoiceModelList = invoiceList.map((e) => InvoiceModel.fromJson(e)).toList();
 
+      invoiceModelList =
+          invoiceList.map((e) => InvoiceModel.fromJson(e)).toList();
     } catch (e) {
       invoiceModelList = <InvoiceModel>[];
     }
